20f0212ca045181589541217d481638a9a21aa35
mkmandal <mkmandal1123@gmail.com>
2023-08-27 11:09:36 +0530

    adding react app code

---
+++ app.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+let demo = React.createElement("h1", { style: { color: "green" }}, "Welcome to Namaste React !!"
+)
+ReactDOM.render(demo,document.getElementById('root')
+);
